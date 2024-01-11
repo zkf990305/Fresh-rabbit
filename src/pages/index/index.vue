f58cf1fd96e579a89151045a8ccf43302811eea8
zkf990305 <sailjamie66@gmail.com>
2024-01-11 17:18:39 +0800

    feat:前台分类组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 
 const bannerList = ref<BannerItem[]>([])
+
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   console.log(res)
   bannerList.value = res.result
 }
+
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  console.log(res)
+  categoryList.value = res.result
+}
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
@@ -29,10 +39,13 @@ onLoad(() => {
   <CustomNavbar />
   <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
-
+  <CategoryPanel :list="categoryList" />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
 //
+page {
+  background-color: #f7f7f7;
+}
 </style>
