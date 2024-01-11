59dc976d277cad2f1e689d077701b356495d25a6
zkf990305 <sailjamie66@gmail.com>
2024-01-11 15:04:30 +0800

    uni.request 请求封装

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
+// 测试请求
+const getData = async () => {
+  const res = await http<number[]>({
+    url: '',
+
+    method: 'GET',
+    header: {},
+  })
+  console.log(res.result)
+}
 </script>
 
 <template>
@@ -11,6 +22,7 @@ const memberStore = useMemberStore()
       @tap="
         memberStore.setProfile({
           nickname: '黑马先锋',
+          token: '1234567890',
         })
       "
       size="mini"
@@ -20,6 +32,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini">测试请求</button>
   </view>
 </template>
 
