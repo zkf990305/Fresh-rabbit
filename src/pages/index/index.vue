c4dc16d6e7aebfbaa1720d5882e03a4930a88418
zkf990305 <sailjamie66@gmail.com>
2024-01-12 10:12:06 +0800

    feat：猜你喜欢类型定义和列表渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -45,10 +45,17 @@ onLoad(() => {
   </uni-card> -->
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <!-- 自定义轮播图 -->
-  <XtxSwiper :list="bannerList" />
-  <CategoryPanel :list="categoryList" />
-  <HotPanel :list="hotList" />
+  <scroll-view class="scroll-view" scroll-y>
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢 -->
+    <XtxGuess />
+  </scroll-view>
+
   <view class="index">index</view>
 </template>
 
@@ -56,5 +63,11 @@ onLoad(() => {
 //
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
