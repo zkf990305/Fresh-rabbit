c3be52dd7ee0d501a39a438497cdd593df38f572
Xiao Zheng <1354597524@qq.com>
2025-01-05 19:05:41 +0800

    feat：地址管理 初始化-获取会员地址数据

--- src/services/address.ts
+++ src/services/address.ts
@@ -12,3 +12,18 @@ export const postMemberAddressAPI = (data: AddressParams) => {
     data,
   })
 }
+
+/**
+ * 获取会员地址信息的API
+ *
+ * 此函数通过发送GET请求到/member/address端点，来获取当前会员的地址信息
+ * 它返回一个Promise对象，解析为AddressParams类型的数组，每个元素代表一个地址信息
+ *
+ * @returns {Promise<AddressParams[]>} 一个Promise，解析为AddressParams类型的数组
+ */
+export const getMemberAddressAPI = () => {
+  return http<AddressParams[]>({
+    method: 'GET',
+    url: '/member/address',
+  })
+}
