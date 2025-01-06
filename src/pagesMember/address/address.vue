51cf42dc7cccb7cfcf3e32adb2ddefd96f6a8692
Xiao Zheng <1354597524@qq.com>
2025-01-06 20:58:51 +0800

    feat：地址管理-新增删除收货地址功能

--- src/pagesMember/address/address.vue
+++ src/pagesMember/address/address.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { getMemberAddressAPI } from '@/services/address'
+import { getMemberAddressAPI, deleteMemberAddressByIdAPI } from '@/services/address'
 import type { AddressItem } from '@/types/address'
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -9,6 +9,21 @@ const addressList = ref<AddressItem[]>([])
 const getMemberAddressData = async () => {
   const res = await getMemberAddressAPI()
   addressList.value = res.result
+}
+
+const onDeleteAddress = (id: string) => {
+  uni.showModal({
+    title: '提示',
+    content: '确定删除该地址吗？',
+    success: async (res) => {
+      if (res.confirm) {
+        // 删除地址
+        await deleteMemberAddressByIdAPI(id)
+        // 刷新列表
+        getMemberAddressData()
+      }
+    },
+  })
 }
 
 // 当页面显示时，获取会员地址数据
@@ -21,10 +36,10 @@ onShow(() => {
   <view class="viewport">
     <!-- 地址列表 -->
     <scroll-view class="scroll-view" scroll-y>
-      <view v-if="true" class="address">
-        <view class="address-list">
+      <view v-if="addressList.length" class="address">
+        <uni-swipe-action class="address-list">
           <!-- 收货地址项 -->
-          <view class="item" v-for="item in addressList" :key="item.id">
+          <uni-swipe-action-item class="item" v-for="item in addressList" :key="item.id">
             <view class="item-content">
               <view class="user">
                 {{ item.receiver }}
@@ -40,8 +55,12 @@ onShow(() => {
                 修改
               </navigator>
             </view>
-          </view>
-        </view>
+            <!-- 右侧插槽 -->
+            <template #right>
+              <button @tap="onDeleteAddress(item.id)" class="delete-button">删除</button>
+            </template>
+          </uni-swipe-action-item>
+        </uni-swipe-action>
       </view>
       <view v-else class="blank">暂无收货地址</view>
     </scroll-view>
