073e8aeb14a978dc4972db007b6aa7a6485951aa
Xiao Zheng <1354597524@qq.com>
2025-01-13 18:35:17 +0800

    feat(cart): 初始化购物车页面
    - 添加购物车页面的基本结构和样式
    - 实现会员登录状态下购物车列表的展示
    - 添加未登录状态下的提示信息
    - 实现购物车商品的滑动操作功能
    - 添加合计金额和去结算按钮
    - 优化页面布局，增加猜你喜欢和底部占位空盒子

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,5 +1,5 @@
 import { http } from '@/utils/http'
-
+import type { CartItem } from '@/types/cart'
 /**
  * 将商品添加到会员购物车的API请求函数
  *
@@ -11,5 +11,20 @@ export const postMemberCartAPI = (data: { skuId: string; count: number }) => {
     method: 'POST', // 使用POST方法发起请求
     url: '/member/cart', // 请求的URL路径
     data, // 请求体数据，包含skuId和count
+  })
+}
+
+/**
+ * 获取会员购物车列表API
+ *
+ * 此函数通过发送GET请求到/member/cart接口，获取当前会员的购物车列表
+ * 它不需要任何参数，因为购物车信息通常是根据当前登录的用户来获取的
+ *
+ * @returns {Promise<CartItem[]>} 返回一个Promise，解析为CartItem类型的数组，代表购物车中的商品列表
+ */
+export const getMemberCartListAPI = () => {
+  return http<CartItem[]>({
+    method: 'GET',
+    url: '/member/cart',
   })
 }
